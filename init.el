e084aa6598660827c6552f0c89a89ccf99e79825
Arul Madhavan <arul.madhavan@arulmadhavan-MBP.local>
2018-05-19 20:16:01 -0700

    Some additions to init.el

--- init.el
+++ init.el
@@ -220,7 +220,7 @@ package-archive-priorities '(("melpa-stable" . 1)))
    company-dabbrev-code-ignore-case nil
    company-dabbrev-downcase nil
    company-idle-delay 0
-   company-minimum-prefix-length 4)
+   company-minimum-prefix-length 2)
   :config
   (define-key company-active-map [tab] nil)
   (define-key company-active-map (kbd "TAB") nil))
@@ -284,27 +284,29 @@ package-archive-priorities '(("melpa-stable" . 1)))
         haskell-indentation-left-offset 4
         haskell-indentation-ifte-offset 4
         haskell-indentation-where-pre-offset 4
-        haskell-indentation-where-post-offset 4
-        haskell-indentation-layout-offset 4))
+        haskell-indentation-where-post-offset 4))
 
 ;; ---------------------------------------------------------------------
 
 ;; Haskell Intero
+;; hindent needs to be installed via stack install hindent
+;; stylish-haskell needs to be installed via stack install stylish-haskell
 (use-package intero
   :init
   (haskell-style-tibbe)
   (setq haskell-stylish-on-save t
-	haskell-tags-on-save t)
+        haskell-tags-on-save t)
   :config
   (add-hook 'haskell-mode-hook 'intero-mode)
-  )
-;; (require 'intero)
-;; (add-hook 'haskell-mode-hook (lambda()
-;; 			       (intero-mode)
-;; 			       (hindent-mode)
-;; 			       ))
+  (add-hook 'haskell-mode-hook 'hindent-mode))
 
 ;; PDF tools install
+(use-package pdf-tools
+  :ensure t
+  :config
+  (custom-set-variables
+    '(pdf-tools-handle-upgrades nil)) ; Use brew upgrade pdf-tools instead.
+  (setq pdf-info-epdfinfo-program "/usr/local/bin/epdfinfo"))
 (pdf-tools-install)
 
 ;; Avy
@@ -511,9 +513,9 @@ package-archive-priorities '(("melpa-stable" . 1)))
 (global-set-key (kbd "C-<backspace>") 'contextual-backspace)
 
 ;; Etags
-(use-package etags-select
-  :ensure t
-  :commands etags-select-find-tag)
+;; (use-package etags-select
+;;   :ensure t
+;;   :commands etags-select-find-tag)
 
 ;; Ensime edit definition
 (defun ensime-edit-definition-with-fallback ()
@@ -670,6 +672,12 @@ package-archive-priorities '(("melpa-stable" . 1)))
 
 (add-hook 'idris-mode-hook #'my-idris-mode-hook))
 
+;; Org mode
+(require 'org)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(setq org-todo-keywords
+      '((sequence "TODO" "INPROGRESS" "WAITING" "DONE")))
+
 ;; (add-hook 'rust-mode-hook #'my-rust-mode-hook)
 ;; (require-package 'nvm)
 
@@ -696,12 +704,14 @@ package-archive-priorities '(("melpa-stable" . 1)))
    (quote
     ("2f0a552a9d14fe8ddaaacdb7b82a0eee1ea1f7f5d0850789915e5b04a1b9669f" "c1390663960169cd92f58aad44ba3253227d8f715c026438303c09b9fb66cdfb" "5e2dc1360a92bb73dafa11c46ba0f30fa5f49df887a8ede4e3533c3ab6270e08" "51e228ffd6c4fff9b5168b31d5927c27734e82ec61f414970fc6bcce23bc140d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "6952b5d43bbd4f1c6727ff61bc9bf5677d385e101433b78ada9c3f0e3787af06" "b9cbfb43711effa2e0a7fbc99d5e7522d8d8c1c151a3194a4b176ec17c9a8215" "4cbec5d41c8ca9742e7c31cc13d8d4d5a18bd3a0961c18eb56d69972bbcf3071" "0b6cb9b19138f9a859ad1b7f753958d8a36a464c6d10550119b2838cedf92171" "ed317c0a3387be628a48c4bbdb316b4fa645a414838149069210b66dd521733f" "6de7c03d614033c0403657409313d5f01202361e35490a3404e33e46663c2596" "938d8c186c4cb9ec4a8d8bc159285e0d0f07bad46edf20aa469a89d0d2a586ea" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "e7aa1c216e14ec250a367908e305dc7f2d549a4b3f87f4a0515ef5d3800fc2f5" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "6f11ad991da959fa8de046f7f8271b22d3a97ee7b6eca62c81d5a917790a45d9" "b378249b7f647796b186c70f61eaaee7aa1bd123681d5ca8c44d3ca8875e1b70" "77ab33a45a8d39566f24cd6a9b28c78eec89882004ed194587e210399813d009" "365d9553de0e0d658af60cff7b8f891ca185a2d7ba3fc6d29aadba69f5194c7f" "611e38c2deae6dcda8c5ac9dd903a356c5de5b62477469133c89b2785eb7a14d" "b81bfd85aed18e4341dbf4d461ed42d75ec78820a60ce86730fc17fc949389b2" default)))
  '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(mac-command-modifier (quote super))
  '(mac-mouse-wheel-mode nil)
- '(mac-option-modifier (quote meta))
+ '(ns-alternate-modifier (quote meta))
+ '(ns-command-modifier (quote super))
+ '(org-agenda-files (quote ("~/.emacs.d/org/tutorials/1.org")))
  '(package-selected-packages
    (quote
-    (company-racer flycheck-rust racer cargo rust-mode smartparens highlight-symbol beacon eshell-prompt-extras treemacs-evil treemacs-projectile real-auto-save origami undo-tree treemacs ensime web-mode json-mode nvm hindent autopair intero darktooth-theme pdf-tools haskell-mode tide magit tern-auto-complete prettier-js company-tern flycheck twilight-bright-theme twilight-anti-bright-theme twilight-theme rjsx-mode helm-projectile monokai-theme moe-theme grandshell-theme ample-theme solarized-theme zenburn-theme xref-js2 which-key use-package try org-bullets js2-refactor helm doom-themes cyberpunk-theme counsel color-theme-sanityinc-tomorrow color-theme auto-complete ace-window))))
+    (pdf-tools company-racer flycheck-rust racer cargo rust-mode smartparens highlight-symbol beacon eshell-prompt-extras treemacs-evil treemacs-projectile real-auto-save origami undo-tree treemacs ensime web-mode json-mode nvm hindent autopair intero darktooth-theme haskell-mode tide magit tern-auto-complete prettier-js company-tern flycheck twilight-bright-theme twilight-anti-bright-theme twilight-theme rjsx-mode helm-projectile monokai-theme moe-theme grandshell-theme ample-theme solarized-theme zenburn-theme xref-js2 which-key use-package try org-bullets js2-refactor helm doom-themes cyberpunk-theme counsel color-theme-sanityinc-tomorrow color-theme auto-complete ace-window)))
+ '(pdf-tools-handle-upgrades nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
